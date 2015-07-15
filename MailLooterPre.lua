bd0890a2cde8e5f425fae9bc55286db076e0e844
Nils Brummond <nils.brummond@gmail.com>
2015-07-15 12:07:51 -0700

    Added documentation.

--- MailLooterPre.lua
+++ MailLooterPre.lua
@@ -2,7 +2,13 @@
 eso_addon_MailLooter = eso_addon_MailLooter or {}
 local ADDON = eso_addon_MailLooter
 
+-- This is a master name (Does not translate)
 ADDON.NAME = "MailLooter"
+
+-- VERSION number format is major.minor
+-- (VERSION * 100) must be an integer number.
 ADDON.VERSION = 0.21
+
+-- Master debug state.
 ADDON.debug = false
 
