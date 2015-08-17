db5fb9a1240b968144bfc1e8fece2c01f3633e1b
Nils Brummond <nils.brummond@gmail.com>
2015-08-17 15:38:09 -0700

    The next release will be 0.29

--- MailLooterPre.lua
+++ MailLooterPre.lua
@@ -7,7 +7,7 @@ ADDON.NAME = "MailLooter"
 
 -- VERSION number format is major.minor
 -- (VERSION * 100) must be an integer number.
-ADDON.VERSION = 0.28
+ADDON.VERSION = 0.29
 
 -- Master debug state.
 ADDON.debug = false
