a86a0eb8cfb9aeae2a8458d40d36aecee572ab74
Nils Brummond <nils.brummond@gmail.com>
2015-09-04 20:03:39 -0700

    Next release will be beta version 0.91.

--- MailLooterPre.lua
+++ MailLooterPre.lua
@@ -7,7 +7,7 @@ ADDON.NAME = "MailLooter"
 
 -- VERSION number format is major.minor
 -- (VERSION * 100) must be an integer number.
-ADDON.VERSION = 0.90
+ADDON.VERSION = 0.91
 
 -- Master debug state.
 ADDON.debug = false
