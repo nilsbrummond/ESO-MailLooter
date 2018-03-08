0e1a9345c5f790c27ac969a93eccfe6d2f4f8c9e
alexesprit <alex.esprit@gmail.com>
2018-03-08 09:39:47 +0300

    Update RU translation
    Fix some spelling mistakes.
    Don't translate addon name in text labels.

--- lang/ru.lua
+++ lang/ru.lua
@@ -5,8 +5,8 @@
 
 local CORE = MailLooter.Core
 -- General
-ZO_CreateStringId("SI_MAILLOOTER_ADDON_NAME", "MailLooter (Сборщик почты)")
-ZO_CreateStringId("SI_MAILLOOTER_TITLE", "MailLooter (Сборщик почты)")
+ZO_CreateStringId("SI_MAILLOOTER_ADDON_NAME", "MailLooter")
+ZO_CreateStringId("SI_MAILLOOTER_TITLE", "MailLooter")
 
 -- There may be up to 6 strings for the defaults.
 MailLooter.defaultAutoReturnSubjects = 
@@ -63,25 +63,25 @@ ZO_CreateStringId("SI_MAILLOOTER_SUMMERY_LOOTING",	"Сбор почты...")
 
 -- Filter Bar Tool Tips
 ZO_CreateStringId("SI_MAILLOOTER_FILTER_ALL",					"Все письма, которые можно собрать")
-ZO_CreateStringId("SI_MAILLOOTER_FILTER_AVA",					"Письма связанные с Войной Альянсов")
-ZO_CreateStringId("SI_MAILLOOTER_FILTER_HIRELING",		"Письма от Ремесленных Наемников")
-ZO_CreateStringId("SI_MAILLOOTER_FILTER_STORE",				"Письма из Гильд.Магазина")
-ZO_CreateStringId("SI_MAILLOOTER_FILTER_COD",					"COD Письма\n(требующие оплату при получении)")
+ZO_CreateStringId("SI_MAILLOOTER_FILTER_AVA",					"Письма, связанные с войной альянсов")
+ZO_CreateStringId("SI_MAILLOOTER_FILTER_HIRELING",		"Письма от ремесленных наемников")
+ZO_CreateStringId("SI_MAILLOOTER_FILTER_STORE",				"Письма из гильд. магазина")
+ZO_CreateStringId("SI_MAILLOOTER_FILTER_COD",					"COD письма\n(требующие оплату при получении)")
 ZO_CreateStringId("SI_MAILLOOTER_FILTER_RETURNED",		"Возвращенные письма")
 ZO_CreateStringId("SI_MAILLOOTER_FILTER_SIMPLE",			"Простые письма")
 ZO_CreateStringId("SI_MAILLOOTER_FILTER_COD_RECEIPT",	"Пришедшая оплата за отправленные вами COD письма")
 
-ZO_CreateStringId("SI_MAILLOOTER_FILTER_HIRELING_SMITH",			"Письма от Наемника-Кузнеца")
-ZO_CreateStringId("SI_MAILLOOTER_FILTER_HIRELING_CLOTH",			"Письма от Наемника-Портного")
-ZO_CreateStringId("SI_MAILLOOTER_FILTER_HIRELING_ENCHANT",		"Письма от Наемника-Зачарователя")
-ZO_CreateStringId("SI_MAILLOOTER_FILTER_HIRELING_PROVISION",	"Письма от Наемника-Снабженца")
-ZO_CreateStringId("SI_MAILLOOTER_FILTER_HIRELING_WOOD",				"Письма от Наемника-Столяра")
+ZO_CreateStringId("SI_MAILLOOTER_FILTER_HIRELING_SMITH",			"Письма от наемника-кузнеца")
+ZO_CreateStringId("SI_MAILLOOTER_FILTER_HIRELING_CLOTH",			"Письма от наемника-портного")
+ZO_CreateStringId("SI_MAILLOOTER_FILTER_HIRELING_ENCHANT",		"Письма от наемника-зачарователя")
+ZO_CreateStringId("SI_MAILLOOTER_FILTER_HIRELING_PROVISION",	"Письма от наемника-снабженца")
+ZO_CreateStringId("SI_MAILLOOTER_FILTER_HIRELING_WOOD",				"Письма от наемника-столяра")
 -- Filter Bar Label
 
 ZO_CreateStringId("SI_MAILLOOTER_FILTER_LABEL_ALL",					"ВСЕ")
-ZO_CreateStringId("SI_MAILLOOTER_FILTER_LABEL_AVA",					"Война Альянсов")
+ZO_CreateStringId("SI_MAILLOOTER_FILTER_LABEL_AVA",					"Война альянсов")
 ZO_CreateStringId("SI_MAILLOOTER_FILTER_LABEL_HIRELING",		"Наемники")
-ZO_CreateStringId("SI_MAILLOOTER_FILTER_LABEL_STORE",				"Гильд.Магазин")
+ZO_CreateStringId("SI_MAILLOOTER_FILTER_LABEL_STORE",				"Гильд. магазин")
 ZO_CreateStringId("SI_MAILLOOTER_FILTER_LABEL_COD",					"COD")
 ZO_CreateStringId("SI_MAILLOOTER_FILTER_LABEL_RETURNED",		"Возвращенные")
 ZO_CreateStringId("SI_MAILLOOTER_FILTER_LABEL_SIMPLE",			"Простые")
@@ -90,7 +90,7 @@ ZO_CreateStringId("SI_MAILLOOTER_FILTER_LABEL_COD_RECEIPT",	"Оплата за C
 ZO_CreateStringId("SI_MAILLOOTER_FILTER_LABEL_SMITH",			"Кузнец")
 ZO_CreateStringId("SI_MAILLOOTER_FILTER_LABEL_CLOTH",			"Портной")
 ZO_CreateStringId("SI_MAILLOOTER_FILTER_LABEL_ENCHANT",		"Зачарователь")
-ZO_CreateStringId("SI_MAILLOOTER_FILTER_LABEL_PROVISION",	"Снабженц")
+ZO_CreateStringId("SI_MAILLOOTER_FILTER_LABEL_PROVISION",	"Снабженец")
 ZO_CreateStringId("SI_MAILLOOTER_FILTER_LABEL_WOOD",			"Столяр")
 
 ZO_CreateStringId("SI_MAILLOOTER_FILTER_LABEL_COMPLEX","Несколько фильтров")
@@ -109,7 +109,7 @@ ZO_CreateStringId("SI_MAILLOOTER_OVERVIEW_COD", "COD:")
 ZO_CreateStringId("SI_MAILLOOTER_OVERVIEW_RETURNED", "Возвращенные:")
 ZO_CreateStringId("SI_MAILLOOTER_OVERVIEW_SIMPLE", "Простые:")
 ZO_CreateStringId("SI_MAILLOOTER_OVERVIEW_COD_RTN", "Оплата за COD:")
-ZO_CreateStringId("SI_MAILLOOTER_OVERVIEW_AUTO_RTN", "Авто-возврат:")
+ZO_CreateStringId("SI_MAILLOOTER_OVERVIEW_AUTO_RTN", "Автовозврат:")
 
 ZO_CreateStringId("SI_MAILLOOTER_OVERVIEW_COD_PAID", "Выплачено за COD:")
 
@@ -121,52 +121,52 @@ ZO_CreateStringId("SI_MAILLOOTER_TIMEOUT",					"Превышено время о
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_AWO", "Настройки общиe нa aккaунт")
 
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_AWO_DESC", 
-"Все параметры на этой странице сохраняются как общие на аккаунт и будут применяться для всех Ваших персонажей.  Только фильтры сбора на закладке Сборцик Почты (не на этой странице) сохраняются отдельно для каждого персонажа.")
+"Все параметры на этой странице сохраняются как общие на аккаунт и будут применяться для всех Ваших персонажей.  Только фильтры сбора на закладке MailLooter (не на этой странице) сохраняются отдельно для каждого персонажа.")
 
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_GO", "Основные настройки")
 
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_RES_IS",		"Зарезервировать (4) места в инвентаре")
-ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_RES_IS_TT",	"Зарезервировать места в инвентаре необходимые для деконструкции предметов на ремесленном станке.")
+ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_RES_IS_TT",	"Зарезервировать места в инвентаре, необходимые для деконструкции предметов на ремесленном станке.")
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_RES_CUS",		"Настроить кол-во зарезервированных мест")
 
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_COD_OPTIONS",	"Настройки COD")
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_COD_ENABLE",	"Оплачивать и собирать COD письма")
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_COD_LIMITS",	"Лимиты для COD писем")
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_COD_L_DESC", 
-"У Сборщика Почты есть два ограничения для COD писем:\n - максимальная стоимость одного письма\n - максимальная стоимость всех писем.\nЗначение |cFF0000НОЛЬ|r отключает ограничение.")
-ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_COD_MAX_PRICE","Макс.стоимость COD письма")
-ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_COD_MAX_COMB_PRICE","Макс.стоимость всех COD писем")
+"У MailLooter есть два ограничения для COD писем:\n - максимальная стоимость одного письма\n - максимальная стоимость всех писем.\nЗначение |cFF0000НОЛЬ|r отключает ограничение.")
+ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_COD_MAX_PRICE","Макс. стоимость COD письма")
+ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_COD_MAX_COMB_PRICE","Макс. стоимость всех COD писем")
 
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_SIMPLE", "Простые письма")
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_SIM_DESC", 
-"Настройки сбора писем от игроков, содержащих предметы или деньги. Письмо определяется как простое основываясь на его теме и тексте сообщения.")
+"Настройки сбора писем от игроков, содержащих предметы или деньги. Письмо определяется как простое, основываясь на его теме и тексте сообщения.")
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_SIM_ENABLE",	"Включить сбор простых писем")
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_SIM_DELETE",	"Удалять простое письмо после сбора")
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_SIM_SUB_T",		"Условия для темы письма")
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_SIM_SUB_DESC", 
-"Считать письмо простым, если кол-во слов в теме меньше либо равно N.  Письма, содержащие в теме слова из списка авто-возврата, указанные ниже, простыми не считаются.")
+"Считать письмо простым, если кол-во слов в теме меньше либо равно N.  Письма, содержащие в теме слова из списка автовозврата, указанные ниже, простыми не считаются.")
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_SIM_SUB_COUNT",	"Кол-во слов в теме")
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_SIM_BODY_T",		"Условия для текста письма")
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_SIM_BODY_DESC", 
-"Считать письмо простым, если кол-во слов в тексте письма меньше либо равно N.")
+"Считать письмо простым, если кол-во слов в тексте письма меньше, либо равно N.")
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_SIM_BODY_COUNT","Кол-во слов в тексте письма")
 
-ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_AR_TITLE","Авто-возврат отправителю")
-ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_AR_DECT",	"Определение писем для Авто-Возврата")
+ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_AR_TITLE","Автовозврат отправителю")
+ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_AR_DECT",	"Определение писем для автовозврата")
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_AR_DECT_DESC",
-"Можно задать до 6 разных слов или фраз.  Если какое(ие) нидудь из них присутствуют в теме письма MailLooter будет считать, что такое письмо надо автоматически вернуть отправителю.  Во время сравнения с заголовком письма: Заглавные и прописные буквы не различаются, пунктуация игнорируется и лишние пробелы также игнорируются.  Оставьте поле 'Тема N' пустым чтобы отключить его.  Аддон будет либо игнорировать, либо автоматически возвращать соответствующие письма.")
+"Можно задать до 6 разных слов или фраз.  Если какое(ие)-нибудь из них присутствуют в теме письма, MailLooter будет считать, что такое письмо надо автоматически вернуть отправителю.  Во время сравнения с заголовком письма заглавные и прописные буквы не различаются, пунктуация игнорируется и лишние пробелы также игнорируются.  Оставьте поле 'Тема N' пустым, чтобы отключить его.  Аддон будет либо игнорировать, либо автоматически возвращать соответствующие письма.")
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_AR_EXAMPLES","Примеры:")
-ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_AR_EX1_T","1. Использовать другой аддон для Авто-Возврата")
+ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_AR_EX1_T","1. Использовать другой аддон для автовозврата")
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_AR_EX1_DESC",
-"Убедитесь, что 'Разрешить Авто-Возврат писем' ОТКЛЮЧЕНО.  Заполните 6 полей 'Тема N' словами, по которым другой аддон определяет Авто-Возвращаемые письма.  MailLooter проигнорирует эти письма, чтобы не мешать работе другого аддона.")
-ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_AR_EX2_T","2. Использовать этот аддон для Авто-Возврата")
+"Убедитесь, что настройка 'Разрешить автовозврат писем' ОТКЛЮЧЕНА.  Заполните 6 полей 'Тема N' словами, по которым другой аддон определяет автовозвращаемые письма.  MailLooter проигнорирует эти письма, чтобы не мешать работе другого аддона.")
+ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_AR_EX2_T","2. Использовать этот аддон для автовозврата")
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_AR_EX2_DESC",
-"Убедитесь, что 'Разрешить Авто-Возврат писем' ВКЛЮЧЕНО.  Заполните 6 полей 'Тема N' словами, по которым вы хотите определять Авто-Возвращаемые письма.  MailLooter автоматически вернет отправителю эти письма во время выполнения сбора почты.")
-ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_AR_EX3_T","3. Не выполнять Авто-Возврат писем")
+"Убедитесь, что настройка 'Разрешить автовозврат писем' ВКЛЮЧЕНА.  Заполните 6 полей 'Тема N' словами, по которым вы хотите определять автовозвращаемые письма.  MailLooter автоматически вернет отправителю эти письма во время выполнения сбора почты.")
+ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_AR_EX3_T","3. Не выполнять автовозврат писем")
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_AR_EX3_DESC",
-"Убедитесь, что 'Разрешить Авто-Возврат писем' ОТКЛЮЧЕНО.  Оставьте 6 полей 'Тема N' пустыми.  MailLooter не будет автоматически возвращать какие-либо письма, также никакие письма не будут считаться подлежащими авто-возврату.")
+"Убедитесь, что настройка 'Разрешить автовозврат писем' ОТКЛЮЧЕНА.  Оставьте 6 полей 'Тема N' пустыми.  MailLooter не будет автоматически возвращать какие-либо письма, также никакие письма не будут считаться подлежащими автовозврату.")
 ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_AR_SUBJECT","Тема <<1>>:")
-ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_AR_ENABLE","Разрешить Авто-Возврат писем")
+ZO_CreateStringId("SI_MAILLOOTER_SETTINGS_AR_ENABLE","Разрешить автовозврат писем")
 
 -- Loot Fragment
 ZO_CreateStringId("SI_MAILLOOTER_LOOT_HEADER_TYPE",     "Тип")
@@ -176,14 +176,14 @@ ZO_CreateStringId("SI_MAILLOOTER_LOOT_HEADER_VALUE",    "Цена")
 ZO_CreateStringId("SI_MAILLOOTER_LOOT_MT_UNKNOWN",			"Письма неизвесного типа")
 ZO_CreateStringId("SI_MAILLOOTER_LOOT_MT_AVA",					"AvA письма")
 ZO_CreateStringId("SI_MAILLOOTER_LOOT_MT_HIRELING",			"Письма от Наемников")
-ZO_CreateStringId("SI_MAILLOOTER_LOOT_MT_STORE",				"Письма из Гильд.Магазина")
+ZO_CreateStringId("SI_MAILLOOTER_LOOT_MT_STORE",				"Письма из гильд. магазина")
 ZO_CreateStringId("SI_MAILLOOTER_LOOT_MT_COD",					"COD письма")
 ZO_CreateStringId("SI_MAILLOOTER_LOOT_MT_RETURNED",			"Возвращенные письма")
 ZO_CreateStringId("SI_MAILLOOTER_LOOT_MT_SIMPLE",				"Письма игроков")
 ZO_CreateStringId("SI_MAILLOOTER_LOOT_MT_COD_RECEIPT",	"Оплаченные COD письма")
 
 ZO_CreateStringId("SI_MAILLOOTER_LOOT_MN_MONEY",				"Деньги")
-ZO_CreateStringId("SI_MAILLOOTER_LOOT_MN_COD_PAYMENT",	"COD Payment")
+ZO_CreateStringId("SI_MAILLOOTER_LOOT_MN_COD_PAYMENT",	"Оплата за COD")
 ZO_CreateStringId("SI_MAILLOOTER_LOOT_MN_RETURNED",			"Возврат денег")
 ZO_CreateStringId("SI_MAILLOOTER_LOOT_MN_SIMPLE",				"Простое письмо")
 
